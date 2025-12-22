84ecd832fd4e839d46b297ebf42dd8693516ab27
Ffinnis <potapovroman03@gmail.com>
2025-12-23 01:11:54 +0700

    feat: remove old version

--- models/__init__.py
+++ models/__init__.py
@@ -2,18 +2,14 @@ from .components import SwiGLUFeedForward
 from .layers import (
     Rotary,
     MultiHeadAttention,
-    TransformerBlock,
 )
 from .llm import MinimalLLM
 from .parallel_block import ParallelTransformerBlock
-from .fast_llm import FastLLM
 
 __all__ = [
     "SwiGLUFeedForward",
     "Rotary",
     "MultiHeadAttention",
-    "TransformerBlock",
     "MinimalLLM",
     "ParallelTransformerBlock",
-    "FastLLM",
 ]
